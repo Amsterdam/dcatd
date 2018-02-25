454ccc80f0653ed90e77c5f7f3426c1129fd4590
Evert Lammerts <evert.lammerts@gmail.com>
2018-02-25 09:36:36 +0100

    allow options

--- src/datacatalog/main.py
+++ src/datacatalog/main.py
@@ -4,9 +4,21 @@ from datacatalog import application
 
 
 def main():
-    aio_app = application.Application()
+    aio_app = application.Application(middlewares=[_handle_options])
     web.run_app(aio_app, port=aio_app.config['web']['port'])
     return 0
+
+
+@web.middleware
+async def _handle_options(request: web.Request, handler):
+    if request.method == 'OPTIONS':
+        return web.Response(
+            text='GET,HEAD,OPTIONS,PATCH,POST,PUT',
+            status=200,
+            headers={
+                'Allow': 'GET,HEAD,OPTIONS,PATCH,POST,PUT'
+            })
+    return await handler(request)
 
 
 if __name__ == '__main__':
