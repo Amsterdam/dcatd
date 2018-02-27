70aebbeb642cbb087fd2bf5054fa8d4f7062c1e8
Evert Lammerts <evert.lammerts@gmail.com>
2018-02-27 15:45:21 +0100

    wipwap

--- src/datacatalog/plugin_interfaces.py
+++ src/datacatalog/plugin_interfaces.py
@@ -162,20 +162,20 @@ def storage_id() -> str:
 ################
 
 # noinspection PyUnusedLocal
-@hookspec.first_only
-def object_store_writer(name,
-                        content_type,
-                        stream) -> T.AsyncContextManager:
-    # language=rst
-    """Asynchronous context manager for file storage.
-
-    The *context* must implement an asynchronous ``write(data: bytes)`` method.
-
-    :param str name: file name under which to store the data
-    :param str content_type: the content type of the file
-    :param aiohttp.StreamReader stream: the data to store
-
-    """
+# @hookspec.first_only
+# def object_store_writer(name,
+#                         content_type,
+#                         stream) -> T.AsyncContextManager:
+#     # language=rst
+#     """Asynchronous context manager for file storage.
+#
+#     The *context* must implement an asynchronous ``write(data: bytes)`` method.
+#
+#     :param str name: file name under which to store the data
+#     :param str content_type: the content type of the file
+#     :param aiohttp.StreamReader stream: the data to store
+#
+#     """
 
 
 ################
