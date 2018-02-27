3fd663884d625ad763658ac22efb689c5b1c6282
Pieter van Beek <pieter@djinnit.com>
2018-02-27 17:04:41 +0100

    uploading

--- src/datacatalog/plugins/swift.py
+++ src/datacatalog/plugins/swift.py
@@ -35,19 +35,23 @@ def initialize_sync(app: web.Application):
     config = app.config['storage_swift']
     _BASE_URL = config['base_url'] + config['container'] + '/'
     _AUTHZ_TOKEN = config['authz_token']
-    _AUTHORIZATION = 'Basic ' + aiohttp.BasicAuth(config['user'], config['password']).encode()
+    _AUTHORIZATION = aiohttp.BasicAuth(config['user'], config['password']).encode()
     app.router.add_post(app['path'] + 'files', post)
 
 
-async def object_store_writer(name: str, content_type: str, data):
+async def object_store_writer(uuid: str, content_type: str,
+                              filename: str, data):
+    _logger.info("url: %s\n", _BASE_URL + uuid)
+    _logger.info("Authorization: %s", _AUTHORIZATION)
+    filename = filename.replace('\\', r'\\').replace('\n', r'\n').replace('"', r'\"')
     async with aiohttp.ClientSession() as session:
-        _logger.info("url: %s\n", _BASE_URL + name)
         async with session.put(
-            _BASE_URL + name,
+            _BASE_URL + uuid,
             data=data,
             headers={
                 'Authorization': _AUTHORIZATION,
-                'Content-Type': content_type
+                'Content-Type': content_type,
+                'Content-Disposition': 'attachment; filename="%s"' % filename
             },
             chunked=True,
             expect100=True
@@ -55,22 +59,27 @@ async def object_store_writer(name: str, content_type: str, data):
             if response.status >= 400:
                 _logger.error(
                     "Couldn't store file in object store.\n"
-                    "%s %s:\n%s", response.status, response.reason,
-                    await response.text()
+                    "%s %s:\n%s\n%r", response.status, response.reason,
+                    await response.text(), response
                 )
                 raise web.HTTPBadGateway()
 
 
 async def post(request: web.Request) -> web.Response:
+
     @aiohttp.streamer
     async def streamer(sink, source):
         while True:
-            chunk = source.readany()
+            chunk = await source.readany()
             if not chunk:
                 break
             await sink.write(chunk)
+
     uuid = uuid4().hex
-    await object_store_writer(uuid, request.content_type, streamer(request.content))
+    await object_store_writer(
+        uuid, request.content_type, 'hallo_wereld.txt',
+        streamer(request.content)
+    )
     return web.Response(
         status=201,
         headers={
