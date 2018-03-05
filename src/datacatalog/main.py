0a62f199d2f22f29ddfc661a9322bce43b9dba58
Evert Lammerts <evert.lammerts@gmail.com>
2018-03-05 11:52:30 +0100

    using uvloop

--- src/datacatalog/main.py
+++ src/datacatalog/main.py
@@ -1,9 +1,13 @@
+import asyncio
+
 from aiohttp import web
+import uvloop
 
 from datacatalog import application
 
 
 def main():
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
     aio_app = application.Application()
     web.run_app(aio_app, port=aio_app.config['web']['port'])
     return 0
