f4bd22ae7854ab9bfc3400abc42d6722eb62f682
Jeroen van 't Hert <jeroen@yottabyte.io>
2018-04-10 18:14:01 +0200

    BAC-994 Fix postgres tests

--- tests/datacatalog/plugins/test_postgres.py
+++ tests/datacatalog/plugins/test_postgres.py
@@ -3,19 +3,18 @@
 import asyncio
 import base64
 import copy
-
-import aiopluggy
-import collections
 import os
 from os import path
+
+import aiopluggy
 import pytest
 
 from datacatalog import config, plugin_interfaces
 from datacatalog.plugins import postgres as postgres_plugin
 
 # set the config file location
-os.environ['CONFIG_PATH'] = os.path.dirname(os.path.abspath(__file__)) + path.sep + 'test_postgres_config.yml'
-
+os.environ['CONFIG_PATH'] = os.path.dirname(
+    os.path.abspath(__file__)) + path.sep + 'test_postgres_config.yml'
 
 _corpus = {
     'dutch_dataset1': {
@@ -45,6 +44,7 @@ _corpus = {
     },
 }
 
+
 class TestApp(dict):
     def __init__(self, pool, loop, config):
         self.pool = pool
@@ -54,7 +54,7 @@ class TestApp(dict):
 
 @pytest.yield_fixture(scope='module')
 def event_loop():
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
     yield loop
     loop.close()
 
@@ -85,7 +85,8 @@ def corpus(event_loop, app):
         for doc_id, record in c.items():
             try:
                 event_loop.run_until_complete(
-                    postgres_plugin.storage_delete(app=app, docid=doc_id, etags={record['etag']})
+                    postgres_plugin.storage_delete(app=app, docid=doc_id,
+                                                   etags={record['etag']})
                 )
             except:
                 pass
@@ -104,7 +105,8 @@ def test_signatures():
 
 
 def test_health_check(event_loop, app):
-    assert event_loop.run_until_complete(postgres_plugin.health_check(app=app)) is None
+    assert event_loop.run_until_complete(
+        postgres_plugin.health_check(app=app)) is None
 
 
 def test_storage_create(corpus):
@@ -127,16 +129,18 @@ def test_storage_retrieve_no_etag(event_loop, corpus, app):
 
 def test_storage_retrieve_with_current_etag(event_loop, corpus, app):
     for doc_id, record in corpus.items():
-        doc, etag = event_loop.run_until_complete(postgres_plugin.storage_retrieve(
-            app=app, docid=doc_id, etags={record['etag']}))
+        doc, etag = event_loop.run_until_complete(
+            postgres_plugin.storage_retrieve(
+                app=app, docid=doc_id, etags={record['etag']}))
         assert doc == None
         assert etag == record['etag']
 
 
 def test_storage_retrieve_with_old_etag(event_loop, corpus, app):
     for doc_id, record in corpus.items():
-        doc, etag = event_loop.run_until_complete(postgres_plugin.storage_retrieve(
-            app=app, docid=doc_id, etags={'oldetag'}))
+        doc, etag = event_loop.run_until_complete(
+            postgres_plugin.storage_retrieve(
+                app=app, docid=doc_id, etags={'oldetag'}))
         assert doc == record['doc']
         assert etag == record['etag']
 
@@ -144,13 +148,18 @@ def test_storage_retrieve_with_old_etag(event_loop, corpus, app):
 def test_storage_extract(event_loop, corpus, app):
     # test ids
     async def retrieve_ids():
-        return [doc_id async for doc_id in postgres_plugin.storage_extract(app, '/properties/id')]
+        return [doc_id async for doc_id in
+                postgres_plugin.storage_extract(app, '/properties/id')]
+
     ids = event_loop.run_until_complete(retrieve_ids())
     assert len(ids) == len(corpus)
     assert len(set(corpus.keys()) - set(ids)) == 0
+
     # test nonexisting
     async def retrieve_nothing():
-        return [doc_id async for doc_id in postgres_plugin.storage_extract(app=app, ptr='/items')]
+        return [doc_id async for doc_id in
+                postgres_plugin.storage_extract(app=app, ptr='/items')]
+
     empty = event_loop.run_until_complete(retrieve_nothing())
     assert len(empty) == 0
 
@@ -162,16 +171,19 @@ def test_search_search(event_loop, corpus, app):
         return [r async for r in postgres_plugin.search_search(
             app=app, q=q, limit=1, offset=None, filters=None,
             iso_639_1_code=record['iso_639_1_code'])]
+
     for doc_id, record in corpus.items():
         for docid, doc in event_loop.run_until_complete(search(record)):
             assert doc == record['doc']
             assert docid == doc_id
+
     # filtered search
     async def search(record):
         filters = {'/properties/id': {'eq': record['doc']['id']}}
         return [r async for r in postgres_plugin.search_search(
             app=app, q='', limit=1, offset=None, filters=filters,
             iso_639_1_code=record['iso_639_1_code'])]
+
     for doc_id, record in corpus.items():
         for docid, doc in event_loop.run_until_complete(search(record)):
             assert doc == record['doc']
