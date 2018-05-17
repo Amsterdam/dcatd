7f380f481f58762f414f51bdaa5b6d095ff21259
Evert Lammerts <evert.lammerts@gmail.com>
2018-05-17 12:48:14 +0200

    openapi shouldnt be cached

--- src/datacatalog/handlers/openapi.py
+++ src/datacatalog/handlers/openapi.py
@@ -6,27 +6,20 @@ from aiohttp import web
 from aiohttp_extras.content_negotiation import produces_content_types
 
 
-_OPENAPI_JSON = None
-"""Cache"""
-
-
 @produces_content_types('application/ld+json', 'application/json')
 async def get(request: web.Request):
     # language=rst
     """Produce the OpenAPI3 definition of this service."""
-    global _OPENAPI_JSON
-
-    if _OPENAPI_JSON is None:
-        openapi_schema = copy.deepcopy(request.app['openapi'])
-        c = request.app.config
-        # add document schema
-        json_schema = await request.app.hooks.mds_json_schema(app=request.app)
-        openapi_schema['components']['schemas']['dcat-doc'] = json_schema
-        # add base url to servers
-        openapi_schema['servers'] = [{'url': c['web']['baseurl']}]
-        _OPENAPI_JSON = json.dumps(openapi_schema, indent='  ', sort_keys=True)
+    openapi_schema = copy.deepcopy(request.app['openapi'])
+    c = request.app.config
+    # add document schema
+    json_schema = await request.app.hooks.mds_json_schema(app=request.app)
+    openapi_schema['components']['schemas']['dcat-doc'] = json_schema
+    # add base url to servers
+    openapi_schema['servers'] = [{'url': c['web']['baseurl']}]
+    openapi_json = json.dumps(openapi_schema, indent='  ', sort_keys=True)
 
     return web.Response(
-        text=_OPENAPI_JSON,
+        text=openapi_json,
         content_type=request['best_content_type']
     )
