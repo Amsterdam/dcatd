1c84858054e99c52c4022b512999b453cf6dbc44
Pieter van Beek <pieter@djinnit.com>
2018-10-21 19:11:56 +0200

    Created new harvesting endpoint /harvest

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -54,7 +54,7 @@ author = 'Amsterdam City Data'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1'
+version = '1.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
