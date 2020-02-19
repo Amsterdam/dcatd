e1ca2f86f4484eece1db4f74a1c988a8dac24776
jjmurre <jan.murre@gmail.com>
2020-02-19 10:04:42 +0100

    Added tests for content of dcat sync.

--- tests/dcatsync/test_sync.py
+++ tests/dcatsync/test_sync.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+from jsonpointer import resolve_pointer
 import pytest
 from dcatsync.sync_aschema import sync
 from amsterdam_schema import types
@@ -28,22 +29,22 @@ def patcher(mocker):
     return set_inputs_and_patch
 
 
-# check which fie is called + with what args
+# check which fie is called
 @pytest.mark.parametrize(
     "dcat_fn, schema_fn, called",
     [
-        ("dcat.json", "afval.json", ("add",)),
-        ("dcat-exist.json", "afval.json", ("update",)),
-        ("dcat-niet.json", "afval.json", ("add",)),
-        ("dcat-two.json", "afval.json", ("delete", "update")),
-        ("dcat-two.json", "bommen.json", ("delete", "add")),
-        ("dcat-ident.json", "afval.json", ()),
-        ("dcat-ident-unsort.json", "afval.json", ()),
-        ("dcat-ident-irrelevant.json", "afval.json", ()),
-        ("dcat.json", "afval-niet.json", ()),
+        ("dcat.json", "afval.json", ("add",)),  # only in incoming
+        ("dcat-exist.json", "afval.json", ("update",)),  # existing in dcat
+        ("dcat-niet.json", "afval.json", ("add",)),  # niet_beschikbaar in dcat
+        ("dcat-two.json", "afval.json", ("delete", "update")),  # afval + dummy in dcat
+        ("dcat-two.json", "bommen.json", ("delete", "add")),  # bommen in aschema
+        ("dcat-ident.json", "afval.json", ()),  # identical in dcat and aschema
+        ("dcat-ident-unsort.json", "afval.json", ()),  # same, but unsorted
+        ("dcat-ident-irrelevant.json", "afval.json", ()),  # irrelevant fields in dcat
+        ("dcat.json", "afval-niet.json", ()),  # afval aschema niet_beschikbaar
     ],
 )
-def test_syncing(patcher, dcat_fn, schema_fn, called):
+def test_syncing_calls(patcher, dcat_fn, schema_fn, called):
     patched = patcher(dcat_fn, schema_fn)
     sync(False, False)
     called_check = {}
@@ -53,3 +54,29 @@ def test_syncing(patcher, dcat_fn, schema_fn, called):
         called_check[fn] = True
     for fn in patched.keys():
         assert patched[fn].called == called_check[fn]
+
+
+# check arguments used when fie is called
+@pytest.mark.parametrize(
+    "dcat_fn, schema_fn, called, arg_pos, checks",
+    [("dcat.json", "afval-more-meta.json", "add", 0, (
+        ("/dct:title", "Afvalwegingen"),
+        ("/overheidds:doel", "Doelstelling"),
+        ("/ams:owner", "De eigenaar"),
+        ("/dct:accrualPeriodicity", "dagelijks"),
+        ("/dcat:contactPoint/vcard:fn", "Tester"),
+        ("/dcat:contactPoint/vcard:hasEmail", "tester@example.com"),
+        ("/dct:temporal/time:hasBeginning", "2018-11-13T20:20:39+00:00"),
+        ("/dct:temporal/time:hasEnd", "2020-12-31T23:59:59+00:00"),
+        ("/dcat:landingPage", "http://example.com"),
+        ("/ams:spatialDescription", "Amsterdam"),
+        ("/ams:spatialUnit", "na"),
+        ("/overheid:grondslag", "De wet"),
+    )),],
+)
+def test_syncing_args(patcher, dcat_fn, schema_fn, called, arg_pos, checks):
+    patched = patcher(dcat_fn, schema_fn)
+    sync(False, False)
+    fie = patched[f"{called}_dataset"]
+    for ptr, val in checks:
+        assert val == resolve_pointer(fie.call_args.args[arg_pos], ptr, None)
