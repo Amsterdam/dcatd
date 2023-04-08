Amsterdam/dcatd

commit 855c45df84e8c919137f235bb7df7bec58db697b

Bump sentry-sdk from 0.14.3 to 1.14.0 (#71)
Bumps [sentry-sdk](https://github.com/getsentry/sentry-python) from 0.14.3 to 1.14.0.
- [Release notes](https://github.com/getsentry/sentry-python/releases)
- [Changelog](https://github.com/getsentry/sentry-python/blob/master/CHANGELOG.md)
- [Commits](https://github.com/getsentry/sentry-python/compare/0.14.3...1.14.0)

---
updated-dependencies:
- dependency-name: sentry-sdk
  dependency-type: direct:production
...

Signed-off-by: dependabot[bot] <support@github.com>
Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

setup.py
@@ -63,7 +63,7 @@ setup(
         "jwcrypto==1.4",
         "pyld==1.0.5",
         "PyYaml==5.4",
-        "sentry-sdk==0.14.3",
+        "sentry-sdk==1.14.0",
         "whoosh==2.7.4",
         "requests==2.23.0",
         # Recommended by aiohttp docs:

commit 47ca7eeec7abca7cd680f395e4b3cab21d162709

Bump requests, pin urllib3
To fix dependency clash caused by #71.

setup.py
@@ -65,7 +65,8 @@ setup(
         "PyYaml==5.4",
         "sentry-sdk==1.14.0",
         "whoosh==2.7.4",
-        "requests==2.23.0",
+        "requests==2.28.2",
+        "urllib3==1.26.15",
         # Recommended by aiohttp docs:
         "aiodns==2.0.0",  # optional asynchronous DNS client
         "uvloop==0.17.0",  # optional fast eventloop for asyncio